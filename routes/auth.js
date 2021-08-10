cf11f23531af622f2de665eb08913400bf804408
Chiragjain55551 <chiragjain55551@gmail.com>
2021-08-10 22:15:09 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -145,7 +145,7 @@ router.post("/register",async(req,res)=>{
                 const isValid = false;
                 const salt = await bcrypt.genSalt();
                 const newPassword = await bcrypt.hash(password, salt);
-                const newUser = new User({uniqueString, email,isValid,password:newPassword,sap_Id, isAdmin:true});
+                const newUser = new User({uniqueString, email,isValid,password:newPassword,sap_Id});
                 await newUser.save();
                 // console.log(newUser)
                 sendEmail(req,email,uniqueString);
