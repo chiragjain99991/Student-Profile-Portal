6a92c8d8666798616d07dec4d20eb1f37a1a0a0c
Chiragjain55551 <chiragjain55551@gmail.com>
2021-08-27 19:18:51 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 let cookieParser = require("cookie-parser");
 const Dbconnect = require('./database')
 let cors = require("cors");
-let dataRoute = require('./routes/Data')
+let dataRoute = require('./routes/data')
 let authRoute = require("./routes/auth")
 
 Dbconnect();
