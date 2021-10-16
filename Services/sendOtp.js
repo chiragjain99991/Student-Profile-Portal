e7872c10a129d6172c73cabd7a17235c95f94dd8
chiragjain99991 <chiragjain55553@gmail.com>
2021-10-16 18:42:25 +0530

    password changed

--- Services/sendOtp.js
+++ Services/sendOtp.js
@@ -7,7 +7,7 @@ module.exports  = (email,otp) => {
         secure: true,
         auth: {
             user: 'djsceItDepartment@gmail.com',
-            pass: 'Pass@123'
+            pass: 'glanrkrzzwzctaam'
           }
       });
 
