84a8b5d4effdbb4f5712a41f5f7b3932b6408a51
Chiragjain55551 <chiragjain99991@gmail.com>
2021-10-19 16:54:07 +0530

    changes

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 function Dbconnect(){
 
-  
+  // mongodb+srv://chiragjain:Chirag123@profileform.7i853.mongodb.net/ProfileForm?retryWrites=true&w=majority
 
 const DB_URL = process.env.DB_URL
     
